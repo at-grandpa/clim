150f8014a4b383a27757cad4e222681094254774
at-grandpa <yoshiyuki.tsuchida.k@gmail.com>
2017-04-05 01:01:55 +0900

    refactoring

--- src/clim/option.cr
+++ src/clim/option.cr
@@ -33,23 +33,28 @@ class Clim
 
     def desc
       desc = @desc
-      desc = desc + "  [default:#{@default}]" unless @default.to_s.empty?
-      desc = desc + "  [required]" if @required
+      desc = desc + "  [default:#{default}]" unless default.to_s.empty?
+      desc = desc + "  [required]" if required
       desc
     end
 
-    def set_value=(arg)
-      @exist = true
-      @value = arg
+    def set_string(@value, @exist = true)
     end
 
-    def add_value(arg)
-      @exist = true
+    def set_bool(arg, @exist = true)
+      if arg.empty?
+        @value = true
+      else
+        raise ClimException.new "Bool arguments accept only \"true\" or \"false\". Input: [#{arg}]" unless arg === "true" || arg == "false"
+        @value = arg === "true"
+      end
+    end
+
+    def add_to_array(arg, @exist = true)
       @value << arg
     end
 
-    def reset
-      @exist = false
+    def reset(@exist = false)
       @value = @default.dup
     end
 
