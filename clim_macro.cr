dac360895acc32f1ff04340dbcc0f88eeee626ef
at-grandpa <yoshiyuki.tsuchida.k@gmail.com>
2017-07-20 05:56:01 +0900

    add sample code

---
+++ clim_macro.cr
@@ -0,0 +1,43 @@
+module M
+  class Opts
+    macro string(short, long)
+      property {{long.gsub(/^-*/, "").gsub(/-/, "_").id}} : String | Nil = nil
+    end
+
+    macro bool(short, long)
+      property {{long.gsub(/^-*/, "").gsub(/-/, "_").id}} : Bool | Nil = nil
+    end
+
+    macro array(short, long)
+      property {{long.gsub(/^-*/, "").gsub(/-/, "_").id}} : Array(String) | Nil = nil
+    end
+  end
+end
+
+module M
+  class C
+    macro options(name)
+      class {{name.camelcase.id}} < Opts
+        {{yield}}
+      end
+
+      def self.{{name.id}}_define
+        opts = {{name.camelcase.id}}.new
+      end
+
+      {{name.id}}_define
+    end
+
+    options(name: "ttt") do
+      string "-s", "--string"
+      bool "-b", "--bool"
+      array "-a", "--array"
+    end
+
+    options(name: "ggg") do
+      string "-s", "--string"
+      bool "-b", "--bool"
+      array "-a", "--array"
+    end
+  end
+end
