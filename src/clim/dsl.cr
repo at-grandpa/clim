23677c8554f3ff95c8b68fd3363eb30a258d2e6d
at-grandpa <yoshiyuki.tsuchida.k@gmail.com>
2017-07-24 08:08:13 +0900

    ...

--- src/clim/dsl.cr
+++ src/clim/dsl.cr
@@ -54,8 +54,8 @@ class Clim
 
   module Dsl
 
-    @@main_command : Command
-    @@defining_command : Command
+    @@main_command : Command = Command.new
+    @@defining_command : Command = Command.new
     @@command_stack : Array(Command) = [] of Command
 
     macro main_command
