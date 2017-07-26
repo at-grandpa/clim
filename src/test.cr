1a5b9532f1a776a973a5b1c6f092c33440868be5
at-grandpa <yoshiyuki.tsuchida.k@gmail.com>
2017-07-27 08:24:59 +0900

    ...

--- src/test.cr
+++ src/test.cr
@@ -11,32 +11,32 @@ module Hello
       run do |opts, args|
         p "main"
         p typeof(opts)
-        p typeof(opts.name)
-        p typeof(opts.greeting)
-        p typeof(opts.web?)
-        p opts.name
-        p opts.greeting
-        p opts.web?
+        # p typeof(opts.name)
+        # p typeof(opts.greeting)
+        # p typeof(opts.web?)
+        # p opts.name
+        # p opts.greeting
+        # p opts.web?
       end
     end
 
     sub do
       command(name: "ttt") do
         desc "Ttt tool."
-        # usage "hello ttt [options] [arguments] ..."
-        array "-n NAME", "--name=NAME", desc: "Target name.", default: [] of String
-        string "-g WORDS", "--greeting=WORDS", desc: "Words of greetings.", default: "Hello"
-        bool "-w", "--web", desc: "Web flag."
-        # run do |opts, args|
-          # p "ttt"
-          # p typeof(opts)
-          # p typeof(opts.name)
-          # p typeof(opts.greeting)
-          # p typeof(opts.web?)
-          # p opts.name
-          # p opts.greeting
-          # p opts.web?
-        # end
+        usage "hello ttt [options] [arguments] ..."
+        array "-b BANANA", "--banana=BANANA", desc: "Banana name.", default: [] of String
+        string "-a APPLE", "--apple=APPLE", desc: "Apple.", default: "Hello"
+        bool "-o", "--orange", desc: "Orange."
+        run do |opts, args|
+          p "ttt"
+          p typeof(opts)
+          # p typeof(opts.banana)
+          # p typeof(opts.apple)
+          # p typeof(opts.orange?)
+          # p opts.banana
+          # p opts.apple
+          # p opts.orange?
+        end
       end
     end
   end
