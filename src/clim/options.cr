7560f9bfbc9e432db0ed3d98edd805656a9691cb
at-grandpa <yoshiyuki.tsuchida.k@gmail.com>
2018-01-05 19:39:04 +0900

    ...

--- src/clim/options.cr
+++ src/clim/options.cr
@@ -12,20 +12,6 @@ class Clim
       opts << opt
     end
 
-    private def opt_validate!(opt)
-      raise ClimException.new "Empty short option." if opt.short_name.empty?
-      raise ClimException.new "Duplicate option. \"-#{opt.short_name}\"" if duplicate_short_name?(opt.short_name)
-      raise ClimException.new "Duplicate option. \"--#{opt.long_name}\"" if duplicate_long_name?(opt.long_name)
-    end
-
-    private def duplicate_short_name?(name)
-      opts.map(&.short_name).includes?(name)
-    end
-
-    private def duplicate_long_name?(name)
-      opts.map(&.long_name).reject(&.empty?).includes?(name)
-    end
-
     def to_h : ReturnOptsType
       hash = ReturnOptsType.new
       hash.merge!({"help" => help})
@@ -41,6 +27,20 @@ class Clim
 
     def validate!
       raise "Required options. \"#{invalid_required_names.join("\", \"")}\"" unless invalid_required_names.empty?
+    end
+
+    private def opt_validate!(opt)
+      raise ClimException.new "Empty short option." if opt.short_name.empty?
+      raise ClimException.new "Duplicate option. \"-#{opt.short_name}\"" if duplicate_short_name?(opt.short_name)
+      raise ClimException.new "Duplicate option. \"--#{opt.long_name}\"" if duplicate_long_name?(opt.long_name)
+    end
+
+    private def duplicate_short_name?(name)
+      opts.map(&.short_name).includes?(name)
+    end
+
+    private def duplicate_long_name?(name)
+      opts.map(&.long_name).reject(&.empty?).includes?(name)
     end
 
     private def invalid_required_names
