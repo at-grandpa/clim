a59f5039b01a99b0b95478d44b71e79816c79790
at-grandpa <yoshiyuki.tsuchida.k@gmail.com>
2018-01-07 11:35:55 +0900

    ...

--- spec/clim/dsl_spec/array_spec.cr
+++ spec/clim/dsl_spec/array_spec.cr
@@ -21,34 +21,28 @@ macro spec(spec_dsl, desc, help_message, spec_cases)
       end
     end
 
+    # spec
     describe {{desc}} do
       describe "if dsl is [" + {{spec_dsl}} + "]," do
-        {% if spec_case.keys.includes?("exception_message".id) %}
-          # exception spec case
-          describe "if argv is " + {{spec_case["argv"].stringify}} + "," do
+        describe "if argv is " + {{spec_case["argv"].stringify}} + "," do
+          {% if spec_case.keys.includes?("exception_message".id) %}
             it "raises an Exception." do
               expect_raises(Exception, {{spec_case["exception_message"]}}) do
                 Case{{index}}.start_main({{spec_case["argv"]}})
               end
             end
-          end
-        {% elsif spec_case.keys.includes?("expect_opts".id) %}
-          # standard spec case
-          describe "if argv is " + {{spec_case["argv"].stringify}} + "," do
+          {% elsif spec_case.keys.includes?("expect_opts".id) %}
             it "opts and args are given as arguments of run block." do
               Case{{index}}.start_main({{spec_case["argv"]}})
             end
-          end
-        {% else %}
-          # display help spec case
-          describe "if argv is " + {{spec_case["argv"].stringify}} + "," do
+          {% else %}
             it "display help." do
               io = IO::Memory.new
               Case{{index}}.start_main({{spec_case["argv"]}}, io)
               io.to_s.should eq {{help_message}}
             end
-          end
-        {% end %}
+          {% end %}
+        end
       end
     end
   {% end %}
@@ -163,151 +157,6 @@ spec(
   ]
 )
 
-# class SpecMainCommandWithArray < Clim
-#  main_command
-#  desc "Main command with desc."
-#  usage "main_command with usage [options] [arguments]"
-#  array "-a ARG", "--array=ARG"
-#  run do |opts, args|
-#  end
-# end
-#
-# describe "main command with array." do
-#  describe "returns help." do
-#    [
-#      {
-#        argv: %w(--help),
-#      },
-#      {
-#        argv: %w(--help ignore-arg),
-#      },
-#      {
-#        argv: %w(ignore-arg --help),
-#      },
-#    ].each do |spec_case|
-#      it "#{spec_case[:argv].join(" ")}" do
-#        run_proc_opts, run_proc_args = SpecMainCommandWithArray.run_proc_arguments(spec_case[:argv])
-#        run_proc_opts["help"].should eq(
-#          <<-HELP_MESSAGE
-#
-#            Main command with desc.
-#
-#            Usage:
-#
-#              main_command with usage [options] [arguments]
-#
-#            Options:
-#
-#              --help                           Show this help.
-#              -a ARG, --array=ARG              Option description.
-#
-#
-#          HELP_MESSAGE
-#        )
-#      end
-#    end
-#  end
-#  describe "returns opts and args when passing argv." do
-#    [
-#      {
-#        argv:        %w(),
-#        expect_opts: create_opts_hash({"array" => nil}),
-#        expect_args: [] of String,
-#      },
-#      {
-#        argv:        %w(arg1),
-#        expect_opts: create_opts_hash({"array" => nil}),
-#        expect_args: ["arg1"],
-#      },
-#      {
-#        argv:        %w(-a array1),
-#        expect_opts: create_opts_hash({"array" => ["array1"]}),
-#        expect_args: [] of String,
-#      },
-#      {
-#        argv:        %w(-aarray1),
-#        expect_opts: create_opts_hash({"array" => ["array1"]}),
-#        expect_args: [] of String,
-#      },
-#      {
-#        argv:        %w(--array array1),
-#        expect_opts: create_opts_hash({"array" => ["array1"]}),
-#        expect_args: [] of String,
-#      },
-#      {
-#        argv:        %w(--array=array1),
-#        expect_opts: create_opts_hash({"array" => ["array1"]}),
-#        expect_args: [] of String,
-#      },
-#      {
-#        argv:        %w(-a array1 arg1),
-#        expect_opts: create_opts_hash({"array" => ["array1"]}),
-#        expect_args: ["arg1"],
-#      },
-#      {
-#        argv:        %w(arg1 -a array1),
-#        expect_opts: create_opts_hash({"array" => ["array1"]}),
-#        expect_args: ["arg1"],
-#      },
-#      {
-#        argv:        %w(-array), # Unintended case.
-#        expect_opts: create_opts_hash({"array" => ["rray"]}),
-#        expect_args: [] of String,
-#      },
-#      {
-#        argv:        %w(-a=array1), # Unintended case.
-#        expect_opts: create_opts_hash({"array" => ["=array1"]}),
-#        expect_args: [] of String,
-#      },
-#    ].each do |spec_case|
-#      it "#{spec_case[:argv].join(" ")}" do
-#        arun_proc_opts, run_proc_args = SpecMainCommandWithArray.run_proc_arguments(spec_case[:argv])
-#        arun_proc_opts.delete("help")
-#        arun_proc_opts.should eq(spec_case[:expect_opts])
-#        run_proc_args.should eq(spec_case[:expect_args])
-#      end
-#    end
-#  end
-#  describe "raises Exception when passing invalid argv." do
-#    [
-#      {
-#        argv:              %w(-h),
-#        exception_message: "Undefined option. \"-h\"",
-#      },
-#      {
-#        argv:              %w(--help -ignore-option),
-#        exception_message: "Undefined option. \"-ignore-option\"",
-#      },
-#      {
-#        argv:              %w(-ignore-option --help),
-#        exception_message: "Undefined option. \"-ignore-option\"",
-#      },
-#      {
-#        argv:              %w(-a),
-#        exception_message: "Option that requires an argument. \"-a\"",
-#      },
-#      {
-#        argv:              %w(--array),
-#        exception_message: "Option that requires an argument. \"--array\"",
-#      },
-#      {
-#        argv:              %w(arg1 -a),
-#        exception_message: "Option that requires an argument. \"-a\"",
-#      },
-#      {
-#        argv:              %w(arg1 --array),
-#        exception_message: "Option that requires an argument. \"--array\"",
-#      },
-#    ].each do |spec_case|
-#      it "#{spec_case[:argv].join(" ")}" do
-#        expect_raises(Exception, spec_case[:exception_message]) do
-#          SpecMainCommandWithArray.run_proc_arguments(spec_case[:argv])
-#        end
-#      end
-#    end
-#  end
-# end
-#
 # class SpecMainCommandWithArrayOnlyShortOption < Clim
 #  main_command
 #  desc "Main command with desc."
