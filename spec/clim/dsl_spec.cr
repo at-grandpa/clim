46db8016de569bed8bd185916c97698d0b5ff169
at-grandpa <yoshiyuki.tsuchida.k@gmail.com>
2018-01-07 13:33:46 +0900

    refactoring array_spec

--- spec/clim/dsl_spec.cr
+++ spec/clim/dsl_spec.cr
@@ -1,7 +1,47 @@
 require "./../spec_helper"
 
-def create_opts_hash(other : Hash(String, String | Bool | Array(String) | Nil) = {} of String => String | Bool | Array(String) | Nil)
-  hash = Clim::ReturnOptsType.new
-  hash.merge!(other)
-  hash
+macro spec(spec_class_name, spec_dsl, spec_desc, help_message, spec_cases)
+  {% for spec_case, index in spec_cases %}
+    {% class_name = (spec_class_name.stringify + index.stringify).id %}
+    # define dsl
+    class {{class_name}} < Clim
+      main_command
+      desc "Main command with desc."
+      usage "main_command with usage [options] [arguments]"
+      {{spec_dsl.id}}
+      run do |opts, args|
+        {% if spec_case.keys.includes?("expect_opts".id) %}
+          opts["help"].should eq {{help_message}}
+          opts.delete("help")
+          opts.should eq Clim::ReturnOptsType.new.merge({{spec_case["expect_opts"]}})
+          args.should eq {{spec_case["expect_args"]}}
+        {% end %}
+      end
+    end
+
+    # spec
+    describe {{spec_desc}} do
+      describe "if dsl is [" + {{spec_dsl}} + "]," do
+        describe "if argv is " + {{spec_case["argv"].stringify}} + "," do
+          {% if spec_case.keys.includes?("expect_opts".id) %}
+            it "opts and args are given as arguments of run block." do
+              {{class_name}}.start_main({{spec_case["argv"]}})
+            end
+          {% elsif spec_case.keys.includes?("exception_message".id) %}
+            it "raises an Exception." do
+              expect_raises(Exception, {{spec_case["exception_message"]}}) do
+                {{class_name}}.start_main({{spec_case["argv"]}})
+              end
+            end
+          {% else %}
+            it "display help." do
+              io = IO::Memory.new
+              {{class_name}}.start_main({{spec_case["argv"]}}, io)
+              io.to_s.should eq {{help_message}}
+            end
+          {% end %}
+        end
+      end
+    end
+  {% end %}
 end
