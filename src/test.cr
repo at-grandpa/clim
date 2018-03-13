f1c612802eb98bf076f912a8e55bb4f031712b2b
at-grandpa <yoshiyuki.tsuchida.k@gmail.com>
2018-03-13 09:31:25 +0900

    ..

--- src/test.cr
+++ src/test.cr
@@ -1,129 +1,34 @@
-class MyCli < ExampleClim
-  class MainCommandByClim < Command
-    @name : String = "main_command_by_clim"
-
-    def name : String
-      @name
-    end
-
-    def name=(name : String)
-      @name = name
-    end
-
-    macro run(&block)
-           def run(str2)
-             RunProc.new {{ block.id }} .call(@options, str2)
-           end
-           end
-
-    def desc : String
-      "\"main command.\""
-    end
-
-    class OptionsByClim
-      property = "Taro"
-    end
-
-    def run(str2)
-      RunProc.new do |options, str2|
-        p("main ---")
-        p(options)
-        p("---")
-      end.call(@options, str2)
-    end
-
-    class SubCommandByClim_Sub1 < Command
-      @name : String = "sub1"
-
-      def name : String
-        @name
-      end
-
-      def name=(name : String)
-        @name = name
-      end
-
-      macro run(&block)
-             def run(str2)
-               RunProc.new {{ block.id }} .call(@options, str2)
-             end
-               end
-
-      def desc : String
-        "\"desc sub1.\""
-      end
-
-      class OptionsByClim
-        property = "Taro"
-      end
-
-      def run(str2)
-        RunProc.new do |options, str2|
-          p("sub1 ---")
-          p(options)
-          p("---")
-        end.call(@options, str2)
-      end
-
-      class OptionsByClim
-      end
-
-      def initialize
-        {% for constant in @type.constants %}
-               {% c = @type.constant(constant) %}
-               {% if c.is_a?(TypeNode) %}
-                 {% if (c.name.split("::")).last == "OptionsByClim" %}
-                   @options = {{ c.id }}.new
-                 {% else %}{% if (c.name.split("::")).last == "RunProc" %}
-                 {% else %}
-                   @sub_commands << {{ c.id }}.new
-                 {% end %}{% end %}
-               {% end %}
-             {% end %}
-      end
-
-      alias RunProc = Proc(OptionsByClim, String, Nil)
-      @options : OptionsByClim = OptionsByClim.new
-
-      def options : OptionsByClim
-        @options
-      end
-
-      def options=(options : OptionsByClim)
-        @options = options
+class C
+  macro my_macro(type)
+    class G({{type}})
+      property var : {{type}}? = nil
+      def set(arg : String)
+        \{% begin %}
+          \{% type_hash = {
+            Int32  => "to_i32",
+            String => "to_s",
+            Bool => "==(\"true\")",
+          } %}
+          \{% p @type.type_vars %}
+          \{% type_ver = @type.type_vars.first %}
+          \{% convert_method = type_hash[type_ver] %}
+          @var = arg.\{{convert_method.id}}
+        \{% end %}
       end
     end
 
-    class OptionsByClim
+    def get_opt
+      G({{type}}).new
     end
-
-    def initialize
-      {% for constant in @type.constants %}
-             {% c = @type.constant(constant) %}
-             {% if c.is_a?(TypeNode) %}
-               {% if (c.name.split("::")).last == "OptionsByClim" %}
-                 @options = {{ c.id }}.new
-               {% else %}{% if (c.name.split("::")).last == "RunProc" %}
-               {% else %}
-                 @sub_commands << {{ c.id }}.new
-               {% end %}{% end %}
-             {% end %}
-           {% end %}
-    end
-
-    alias RunProc = Proc(OptionsByClim, String, Nil)
-    @options : OptionsByClim = OptionsByClim.new
-
-    def options : OptionsByClim
-      @options
-    end
-
-    def options=(options : OptionsByClim)
-      @options = options
-    end
-  end
-
-  def self.start(argv)
-    (MainCommandByClim.new.parse(argv)).run("bbb")
   end
 end
+
+class M < C
+  my_macro(Bool)
+end
+
+m = M.new
+g = m.get_opt
+g.set("true")
+p g.var
+p g.var.class
