85504688f792526d68e5721b5db7c1d8f7f56685
at-grandpa <yoshiyuki.tsuchida.k@gmail.com>
2018-03-15 09:59:40 +0900

    pass

--- src/clim/command/macro.cr
+++ src/clim/command/macro.cr
@@ -94,6 +94,30 @@ class Clim
                   @value = arg.\{{convert_method.id}}
                 \{% end %}
               end
+
+              def desc
+                desc = @desc
+                # desc = desc + "  [type:#{T.to_s}]" unless default.nil?
+                desc = desc + "  [default:#{display_default}]" unless default.nil?
+                desc = desc + "  [required]" if required
+                desc
+              end
+
+              private def display_default
+                default_value = default
+                case default_value
+                when String
+                  default_value.empty? ? "\"\"" : "\"#{default}\""
+                when Bool
+                  default_value
+                when Array(String)
+                  default_value.empty? ? "[] of String" : default
+                when Nil
+                  "nil"
+                else
+                  raise Exception.new "'default' type is not supported. default type is [#{typeof(default)}]"
+                end
+              end
             end
           end
 
