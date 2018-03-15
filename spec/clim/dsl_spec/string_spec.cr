613054600fc67c85a2317176e1fb9009a56419dd
at-grandpa <yoshiyuki.tsuchida.k@gmail.com>
2018-03-15 09:46:16 +0900

    ng option desc with default value

--- spec/clim/dsl_spec/string_spec.cr
+++ spec/clim/dsl_spec/string_spec.cr
@@ -461,179 +461,219 @@ spec(
   ]
 )
 {% end %}
-#
-# {% begin %}
-# {%
-#   main_help_message = <<-HELP_MESSAGE
-#
-#                         Command Line Interface Tool.
-#
-#                         Usage:
-#
-#                           main_command [options] [arguments]
-#
-#                         Options:
-#
-#                           --help                           Show this help.
-#                           -s ARG, --string=ARG             String option description.
-#
-#
-#                       HELP_MESSAGE
-# %}
-#
-# spec(
-#   spec_class_name: MainCommandWithStringDesc,
-#   spec_dsl_lines: [
-#     "string \"-s ARG\", \"--string=ARG\", desc: \"String option description.\"",
-#   ],
-#   spec_desc: "main command with string dsl,",
-#   spec_cases: [
-#     {
-#       argv:        ["--help"],
-#       expect_help: {{main_help_message}},
-#     },
-#     {
-#       argv:        ["--help", "ignore-arg"],
-#       expect_help: {{main_help_message}},
-#     },
-#     {
-#       argv:        ["ignore-arg", "--help"],
-#       expect_help: {{main_help_message}},
-#     },
-#   ]
-# )
-# {% end %}
-#
-# {% begin %}
-# {%
-#   main_help_message = <<-HELP_MESSAGE
-#
-#                         Command Line Interface Tool.
-#
-#                         Usage:
-#
-#                           main_command [options] [arguments]
-#
-#                         Options:
-#
-#                           --help                           Show this help.
-#                           -s ARG, --string=ARG             String option description.  [default:"default value"]
-#
-#
-#                       HELP_MESSAGE
-# %}
-#
-# spec(
-#   spec_class_name: MainCommandWithStringDefault,
-#   spec_dsl_lines: [
-#     "string \"-s ARG\", \"--string=ARG\", desc: \"String option description.\", default: \"default value\"",
-#   ],
-#   spec_desc: "main command with string dsl,",
-#   spec_cases: [
-#     {
-#       argv:        [] of String,
-#       expect_help: {{main_help_message}},
-#       expect_opts: {"string" => "default value"},
-#       expect_args: [] of String,
-#     },
-#     {
-#       argv:        ["arg1"],
-#       expect_help: {{main_help_message}},
-#       expect_opts: {"string" => "default value"},
-#       expect_args: ["arg1"],
-#     },
-#     {
-#       argv:        ["-s", "string1"],
-#       expect_help: {{main_help_message}},
-#       expect_opts: {"string" => "string1"},
-#       expect_args: [] of String,
-#     },
-#     {
-#       argv:        ["-sstring1"],
-#       expect_help: {{main_help_message}},
-#       expect_opts: {"string" => "string1"},
-#       expect_args: [] of String,
-#     },
-#     {
-#       argv:        ["--string", "string1"],
-#       expect_help: {{main_help_message}},
-#       expect_opts: {"string" => "string1"},
-#       expect_args: [] of String,
-#     },
-#     {
-#       argv:        ["--string=string1"],
-#       expect_help: {{main_help_message}},
-#       expect_opts: {"string" => "string1"},
-#       expect_args: [] of String,
-#     },
-#     {
-#       argv:        ["-s", "string1", "arg1"],
-#       expect_help: {{main_help_message}},
-#       expect_opts: {"string" => "string1"},
-#       expect_args: ["arg1"],
-#     },
-#     {
-#       argv:        ["arg1", "-s", "string1"],
-#       expect_help: {{main_help_message}},
-#       expect_opts: {"string" => "string1"},
-#       expect_args: ["arg1"],
-#     },
-#     {
-#       argv:        ["-string"], # Unintended case.
-#       expect_help: {{main_help_message}},
-#       expect_opts: {"string" => "tring"},
-#       expect_args: [] of String,
-#     },
-#     {
-#       argv:        ["-s=string1"], # Unintended case.
-#       expect_help: {{main_help_message}},
-#       expect_opts: {"string" => "=string1"},
-#       expect_args: [] of String,
-#     },
-#     {
-#       argv:              ["-h"],
-#       exception_message: "Undefined option. \"-h\"",
-#     },
-#     {
-#       argv:              ["--help", "-ignore-option"],
-#       exception_message: "Undefined option. \"-ignore-option\"",
-#     },
-#     {
-#       argv:              ["-ignore-option", "--help"],
-#       exception_message: "Undefined option. \"-ignore-option\"",
-#     },
-#     {
-#       argv:              ["-s"],
-#       exception_message: "Option that requires an argument. \"-s\"",
-#     },
-#     {
-#       argv:              ["--string"],
-#       exception_message: "Option that requires an argument. \"--string\"",
-#     },
-#     {
-#       argv:              ["arg1", "-s"],
-#       exception_message: "Option that requires an argument. \"-s\"",
-#     },
-#     {
-#       argv:              ["arg1", "--string"],
-#       exception_message: "Option that requires an argument. \"--string\"",
-#     },
-#     {
-#       argv:        ["--help"],
-#       expect_help: {{main_help_message}},
-#     },
-#     {
-#       argv:        ["--help", "ignore-arg"],
-#       expect_help: {{main_help_message}},
-#     },
-#     {
-#       argv:        ["ignore-arg", "--help"],
-#       expect_help: {{main_help_message}},
-#     },
-#   ]
-# )
-# {% end %}
-#
+
+{% begin %}
+{%
+  main_help_message = <<-HELP_MESSAGE
+
+                        Command Line Interface Tool.
+
+                        Usage:
+
+                          main_command [options] [arguments]
+
+                        Options:
+
+                          -s ARG, --string=ARG             String option description.
+                          --help                           Show this help.
+
+
+                      HELP_MESSAGE
+%}
+
+spec(
+  spec_class_name: MainCommandWithStringDesc,
+  spec_dsl_lines: [
+    "options \"-s ARG\", \"--string=ARG\", type: String, desc: \"String option description.\"",
+  ],
+  spec_desc: "main command with string dsl,",
+  spec_cases: [
+    {
+      argv:        ["--help"],
+      expect_help: {{main_help_message}},
+    },
+    {
+      argv:        ["--help", "ignore-arg"],
+      expect_help: {{main_help_message}},
+    },
+    {
+      argv:        ["ignore-arg", "--help"],
+      expect_help: {{main_help_message}},
+    },
+  ]
+)
+{% end %}
+
+{% begin %}
+{%
+  main_help_message = <<-HELP_MESSAGE
+
+                        Command Line Interface Tool.
+
+                        Usage:
+
+                          main_command [options] [arguments]
+
+                        Options:
+
+                          -s ARG, --string=ARG             String option description.  [default:"default value"]
+                          --help                           Show this help.
+
+
+                      HELP_MESSAGE
+%}
+
+spec(
+  spec_class_name: MainCommandWithStringDefault,
+  spec_dsl_lines: [
+    "options \"-s ARG\", \"--string=ARG\", type: String, desc: \"String option description.\", default: \"default value\"",
+  ],
+  spec_desc: "main command with string dsl,",
+  spec_cases: [
+    {
+      argv:        [] of String,
+      expect_help: {{main_help_message}},
+      expect_opts: {
+        "type" => String?,
+        "method" => "string",
+        "expect_value" => "default value",
+      },
+      expect_args: [] of String,
+    },
+    {
+      argv:        ["arg1"],
+      expect_help: {{main_help_message}},
+      expect_opts: {
+        "type" => String?,
+        "method" => "string",
+        "expect_value" => "default value",
+      },
+      expect_args: ["arg1"],
+    },
+    {
+      argv:        ["-s", "string1"],
+      expect_help: {{main_help_message}},
+      expect_opts: {
+        "type" => String?,
+        "method" => "string",
+        "expect_value" => "string1",
+      },
+      expect_args: [] of String,
+    },
+    {
+      argv:        ["-sstring1"],
+      expect_help: {{main_help_message}},
+      expect_opts: {
+        "type" => String?,
+        "method" => "string",
+        "expect_value" => "string1",
+      },
+      expect_args: [] of String,
+    },
+    {
+      argv:        ["--string", "string1"],
+      expect_help: {{main_help_message}},
+      expect_opts: {
+        "type" => String?,
+        "method" => "string",
+        "expect_value" => "string1",
+      },
+      expect_args: [] of String,
+    },
+    {
+      argv:        ["--string=string1"],
+      expect_help: {{main_help_message}},
+      expect_opts: {
+        "type" => String?,
+        "method" => "string",
+        "expect_value" => "string1",
+      },
+      expect_args: [] of String,
+    },
+    {
+      argv:        ["-s", "string1", "arg1"],
+      expect_help: {{main_help_message}},
+      expect_opts: {
+        "type" => String?,
+        "method" => "string",
+        "expect_value" => "string1",
+      },
+      expect_args: ["arg1"],
+    },
+    {
+      argv:        ["arg1", "-s", "string1"],
+      expect_help: {{main_help_message}},
+      expect_opts: {
+        "type" => String?,
+        "method" => "string",
+        "expect_value" => "string1",
+      },
+      expect_args: ["arg1"],
+    },
+    {
+      argv:        ["-string"], # Unintended case.
+      expect_help: {{main_help_message}},
+      expect_opts: {
+        "type" => String?,
+        "method" => "string",
+        "expect_value" => "tring",
+      },
+      expect_args: [] of String,
+    },
+    {
+      argv:        ["-s=string1"], # Unintended case.
+      expect_help: {{main_help_message}},
+      expect_opts: {
+        "type" => String?,
+        "method" => "string",
+        "expect_value" => "=string1",
+      },
+      expect_args: [] of String,
+    },
+    {
+      argv:              ["-h"],
+      exception_message: "Undefined option. \"-h\"",
+    },
+    {
+      argv:              ["--help", "-ignore-option"],
+      exception_message: "Undefined option. \"-ignore-option\"",
+    },
+    {
+      argv:              ["-ignore-option", "--help"],
+      exception_message: "Undefined option. \"-ignore-option\"",
+    },
+    {
+      argv:              ["-s"],
+      exception_message: "Option that requires an argument. \"-s\"",
+    },
+    {
+      argv:              ["--string"],
+      exception_message: "Option that requires an argument. \"--string\"",
+    },
+    {
+      argv:              ["arg1", "-s"],
+      exception_message: "Option that requires an argument. \"-s\"",
+    },
+    {
+      argv:              ["arg1", "--string"],
+      exception_message: "Option that requires an argument. \"--string\"",
+    },
+    {
+      argv:        ["--help"],
+      expect_help: {{main_help_message}},
+    },
+    {
+      argv:        ["--help", "ignore-arg"],
+      expect_help: {{main_help_message}},
+    },
+    {
+      argv:        ["ignore-arg", "--help"],
+      expect_help: {{main_help_message}},
+    },
+  ]
+)
+{% end %}
+
 # {% begin %}
 # {%
 #   main_help_message = <<-HELP_MESSAGE
