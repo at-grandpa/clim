87523a625cf69285092be7f9288c8828b6a991f6
at-grandpa <yoshiyuki.tsuchida.k@gmail.com>
2018-03-16 18:06:47 +0900

    pass

--- src/sample.cr
+++ src/sample.cr
@@ -15,48 +15,9 @@ require "./clim"
 # [done] 同じコマンドを指定した場合の動作
 #   - macro raise でキャッチ
 # [done] サポートしていない型だったらエラー
-# 型が違った場合の挙動
+# [done] 型が違った場合の挙動
+#   - 難し良いので諦め
 # モジュールの切り出し
-
-# class MyCli < Clim
-#  main_command do
-#    desc "main command."
-#    option "-n", "--name=NAME", type: String, desc: "your name.", default: "Taro", required: true
-#    option "-t", "--time=TIME", type: Int32, desc: "your time.", default: 4, required: true
-#    run do |options, arguments|
-#      p "main ---"
-#      p options.name
-#      p options.time
-#      p arguments
-#      p "---"
-#    end
-#    sub_command "sub1" do
-#      desc "desc sub1."
-#      option "-n", "--name=NAME", type: String, desc: "your name.", default: "Taro", required: true
-#      option "-t", "--time=TIME", type: Int32, desc: "your time.", default: 4, required: true
-#      run do |options, arguments|
-#        p "sub1 ---"
-#        p options.name
-#        p options.time
-#        p "---"
-#      end
-#      sub_command "subsub1" do
-#        desc "desc subsub1."
-#        option "-n", "--name=NAME", type: String, desc: "your name.", default: "Taro", required: true
-#        option "-t", "--time=TIME", type: Int32, desc: "your time.", default: 4, required: true
-#        option "-b", "--bool", type: Bool, desc: "your bool.", default: false, required: true
-#        run do |options, arguments|
-#          p "subsub1 ---"
-#          p options.name
-#          p options.time
-#          p options.bool
-#          p arguments
-#          p "---"
-#        end
-#      end
-#    end
-#  end
-# end
 
 class MyCli < Clim
   main_command do
