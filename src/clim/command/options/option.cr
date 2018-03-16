576f8b1c7ec7e340b43a2bd8209fb4a1cd473dcd
at-grandpa <yoshiyuki.tsuchida.k@gmail.com>
2018-03-17 02:57:16 +0900

    pass

--- src/clim/command/options/option.cr
+++ src/clim/command/options/option.cr
@@ -8,12 +8,6 @@ class Clim
         property required : Bool = false
         property array_set_flag : Bool = false
 
-        macro add_array_value(type, cast_method)
-          @value = [] of {{type}} if @array_set_flag == false
-          @array_set_flag = true
-          @value = @value.nil? ? [arg.{{cast_method}}] : @value.try &.<<(arg.{{cast_method}})
-        end
-
         def required_set? : Bool
           @required && @value.nil?
         end
@@ -68,42 +62,68 @@ class Clim
           end
 
           def set_value(arg : String)
-            {% type_hash = {
-                 "Int8"    => "@value = arg.to_i8",
-                 "Int16"   => "@value = arg.to_i16",
-                 "Int32"   => "@value = arg.to_i32",
-                 "Int64"   => "@value = arg.to_i64",
-                 "UInt8"   => "@value = arg.to_u8",
-                 "UInt16"  => "@value = arg.to_u16",
-                 "UInt32"  => "@value = arg.to_u32",
-                 "UInt64"  => "@value = arg.to_u64",
-                 "Float32" => "@value = arg.to_f32",
-                 "Float64" => "@value = arg.to_f64",
-                 "String"  => "@value = arg.to_s",
-                 "Bool"    => <<-BOOL_ARG
-    @value = arg.try do |obj|
-      next true if obj.empty?
-      unless obj === "true" || obj == "false"
-        raise ClimException.new "Bool arguments accept only \\"true\\" or \\"false\\". Input: [\#{obj}]"
-      end
-      obj === "true"
-    end
-  BOOL_ARG,
-                 "Array(Int8)"    => "add_array_value(Int8, to_i8)",
-                 "Array(Int16)"   => "add_array_value(Int16, to_i16)",
-                 "Array(Int32)"   => "add_array_value(Int32, to_i32)",
-                 "Array(Int64)"   => "add_array_value(Int64, to_i64)",
-                 "Array(UInt8)"   => "add_array_value(UInt8, to_u8)",
-                 "Array(UInt16)"  => "add_array_value(UInt16, to_u16)",
-                 "Array(UInt32)"  => "add_array_value(UInt32, to_u32)",
-                 "Array(UInt64)"  => "add_array_value(UInt64, to_u64)",
-                 "Array(Float32)" => "add_array_value(Float32, to_f32)",
-                 "Array(Float64)" => "add_array_value(Float64, to_f64)",
-                 "Array(String)"  => "add_array_value(String, to_s)",
-               } %}
-            {% convert_method = type_hash[type.stringify] %}
-            {{convert_method.id}}
+            {% if type.id == Int8.id %}
+              @value = arg.to_i8
+            {% elsif type.id == Int16.id %}
+              @value = arg.to_i16
+            {% elsif type.id == Int32.id %}
+              @value = arg.to_i32
+            {% elsif type.id == Int64.id %}
+              @value = arg.to_i64
+            {% elsif type.id == UInt8.id %}
+              @value = arg.to_u8
+            {% elsif type.id == UInt16.id %}
+              @value = arg.to_u16
+            {% elsif type.id == UInt32.id %}
+              @value = arg.to_u32
+            {% elsif type.id == UInt64.id %}
+              @value = arg.to_u64
+            {% elsif type.id == Float32.id %}
+              @value = arg.to_f32
+            {% elsif type.id == Float64.id %}
+              @value = arg.to_f64
+            {% elsif type.id == String.id %}
+              @value = arg.to_s
+            {% elsif type.id == Bool.id %}
+              @value = arg.try do |obj|
+                next true if obj.empty?
+                unless obj === "true" || obj == "false"
+                  raise ClimException.new "Bool arguments accept only \"true\" or \"false\". Input: [#{obj}]"
+                end
+                obj === "true"
+              end
+            {% elsif type.id == "Array(Int8)".id %}
+              add_array_value(Int8, to_i8)
+            {% elsif type.id == "Array(Int16)".id %}
+              add_array_value(Int16, to_i16)
+            {% elsif type.id == "Array(Int32)".id %}
+              add_array_value(Int32, to_i32)
+            {% elsif type.id == "Array(Int64)".id %}
+              add_array_value(Int64, to_i64)
+            {% elsif type.id == "Array(UInt8)".id %}
+              add_array_value(UInt8, to_u8)
+            {% elsif type.id == "Array(UInt16)".id %}
+              add_array_value(UInt16, to_u16)
+            {% elsif type.id == "Array(UInt32)".id %}
+              add_array_value(UInt32, to_u32)
+            {% elsif type.id == "Array(UInt64)".id %}
+              add_array_value(UInt64, to_u64)
+            {% elsif type.id == "Array(Float32)".id %}
+              add_array_value(Float32, to_f32)
+            {% elsif type.id == "Array(Float64)".id %}
+              add_array_value(Float64, to_f64)
+            {% elsif type.id == "Array(String)".id %}
+              add_array_value(String, to_s)
+            {% else %}
+              {% raise "Type [#{type}] is not supported on option." %}
+            {% end %}
           end
+        end
+
+        macro add_array_value(type, cast_method)
+          @value = [] of {{type}} if @array_set_flag == false
+          @array_set_flag = true
+          @value = @value.nil? ? [arg.{{cast_method}}] : @value.try &.<<(arg.{{cast_method}})
         end
       end
     end
