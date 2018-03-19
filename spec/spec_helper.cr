7596588f58a272eeead907b7cc2e76f35b801bd2
at-grandpa <yoshiyuki.tsuchida.k@gmail.com>
2018-03-19 22:28:45 +0900

    fix

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,11 +1,2 @@
 require "spec"
 require "../src/clim"
-require "../src/clim/command"
-# require "../src/clim/option"
-# require "../src/clim/options"
-# require "../src/clim/dsl"
-
-alias Command = Clim::Command
-# alias Option = Clim::Option
-# alias Options = Clim::Options
-# alias Dsl = Clim::Dsl
