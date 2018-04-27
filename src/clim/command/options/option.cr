01a517264812f09babb43519612cb2b689688562
at-grandpa <yoshiyuki.tsuchida.k@gmail.com>
2018-04-27 20:39:57 +0900

    ...

--- src/clim/command/options/option.cr
+++ src/clim/command/options/option.cr
@@ -42,24 +42,28 @@ class Clim
           {% if default != nil && required == true %}
             {% value_type = type.stringify %}
             {% value_default_value = default %}
+            {% value_default_assign = "default".id %}
             {% default_type = type.stringify %}
             {% default_value = default %}
             {% display_default_on_help_flag = true %}
           {% elsif default != nil && required == false %}
             {% value_type = type.stringify %}
             {% value_default_value = default %}
+            {% value_default_assign = "default".id %}
             {% default_type = type.stringify %}
             {% default_value = default %}
             {% display_default_on_help_flag = true %}
           {% elsif default == nil && required == true %}
             {% value_type = type.stringify %}
             {% value_default_value = SUPPORT_TYPES_ALL_HASH[type][:default] %}
+            {% value_default_assign = SUPPORT_TYPES_ALL_HASH[type][:default] %}
             {% default_type = type.stringify + "?" %}
             {% default_value = default %}
             {% display_default_on_help_flag = false %}
           {% elsif default == nil && required == false %}
             {% value_type = type.stringify + "?" %}
             {% value_default_value = default %}
+            {% value_default_assign = "default".id %}
             {% default_type = type.stringify + "?" %}
             {% default_value = default %}
             {% display_default_on_help_flag = false %}
@@ -71,20 +75,12 @@ class Clim
           property display_default_on_help_flag : Bool = {{display_default_on_help_flag.id}}
 
           def initialize(@short : String, @long : String, @desc : String, @default : {{default_type.id}}, @required : Bool)
-            {% if default == nil && required == true %}
-              @value = {{SUPPORT_TYPES_ALL_HASH[type][:default]}}
-            {% else %}
-              @value = default
-            {% end %}
+              @value = {{value_default_assign}}
           end
 
           def initialize(@short : String, @desc : String, @default : {{default_type.id}}, @required : Bool)
             @long = nil
-            {% if default == nil && required == true %}
-              @value = {{SUPPORT_TYPES_ALL_HASH[type][:default]}}
-            {% else %}
-              @value = default
-            {% end %}
+            @value = {{value_default_assign}}
           end
 
           def desc
