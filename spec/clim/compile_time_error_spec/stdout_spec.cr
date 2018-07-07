1966af6f9e0d60fabc599335f7e9c626a2226a5e
at-grandpa <yoshiyuki.tsuchida.k@gmail.com>
2018-07-07 21:57:17 +0900

    ...

--- spec/clim/compile_time_error_spec/stdout_spec.cr
+++ spec/clim/compile_time_error_spec/stdout_spec.cr
@@ -17,4 +17,25 @@ describe "STDOUT spec, " do
 
     DISPLAY
   end
+  it "display main_command help." do
+    `crystal run spec/clim/compile_time_error_spec/files/main_command_default_help.cr --no-color -- sub_command --help`.should eq <<-DISPLAY
+
+      sub_comand.
+
+      Usage:
+
+        sub_command [options] [arguments]
+
+      Options:
+
+        -n NUM                           Number. [type:Int32] [default:0]
+        --help                           Show this help.
+
+      Sub Commands:
+
+        sub_sub_command   sub_sub_comand description.
+
+
+    DISPLAY
+  end
 end
