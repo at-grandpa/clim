ecfc3ab2878ea7bd0c56476deae0ddba9f6588c1
at-grandpa <yoshiyuki.tsuchida.k@gmail.com>
2020-05-04 11:30:53 +0900

    float

---
+++ spec/clim/dsl_spec/argument_types/float_spec.cr
@@ -0,0 +1,146 @@
+require "../../dsl_spec"
+
+{% begin %}
+{%
+  main_help_message = <<-HELP_MESSAGE
+
+                        Command Line Interface Tool.
+
+                        Usage:
+
+                          main_command_of_clim_library [options] [arguments]
+
+                        Options:
+
+                          -a ARG, --array=ARG              Option description. [type:Array(String)]
+                          --help                           Show this help.
+
+                        Arguments:
+
+                          01. arg-float-32      float32 argument. [type:Float32]
+                          02. arg-float-64      float64 argument. [type:Float64]
+
+
+                      HELP_MESSAGE
+%}
+
+spec(
+  spec_class_name: ArgumentTypeSpec,
+  spec_dsl_lines: [
+    "argument \"arg-float-32\", type: Float32, desc: \"float32 argument.\"",
+    "argument \"arg-float-64\", type: Float64, desc: \"float64 argument.\"",
+    "option \"-a ARG\", \"--array=ARG\", type: Array(String)",
+  ],
+  spec_desc: "argument type spec,",
+  spec_cases: [
+    # ====================================================
+    # Float
+    # ====================================================
+    {
+      argv:        [] of String,
+      expect_help: {{main_help_message}},
+      expect_args: [
+        {
+          "type" => Float32?,
+          "method" => "arg_float_32",
+          "expect_value" => nil,
+        },
+        {
+          "type" => Float64?,
+          "method" => "arg_float_64",
+          "expect_value" => nil,
+        },
+        {
+          "type" => Array(String),
+          "method" => "unknown_args",
+          "expect_value" => [] of String,
+        },
+      ],
+    },
+    {
+      argv:        ["1.1"],
+      expect_help: {{main_help_message}},
+      expect_args: [
+        {
+          "type" => Float32?,
+          "method" => "arg_float_32",
+          "expect_value" => 1.1f32,
+        },
+        {
+          "type" => Float64?,
+          "method" => "arg_float_64",
+          "expect_value" => nil,
+        },
+        {
+          "type" => Array(String),
+          "method" => "unknown_args",
+          "expect_value" => [] of String,
+        },
+      ],
+    },
+    {
+      argv:        ["1.1", "2.2"],
+      expect_help: {{main_help_message}},
+      expect_args: [
+        {
+          "type" => Float32?,
+          "method" => "arg_float_32",
+          "expect_value" => 1.1f32,
+        },
+        {
+          "type" => Float64?,
+          "method" => "arg_float_64",
+          "expect_value" => 2.2f64,
+        },
+        {
+          "type" => Array(String),
+          "method" => "unknown_args",
+          "expect_value" => [] of String,
+        },
+      ],
+    },
+    {
+      argv:        ["1.1", "2.2", "3.3"],
+      expect_help: {{main_help_message}},
+      expect_args: [
+        {
+          "type" => Float32?,
+          "method" => "arg_float_32",
+          "expect_value" => 1.1f32,
+        },
+        {
+          "type" => Float64?,
+          "method" => "arg_float_64",
+          "expect_value" => 2.2f64,
+        },
+        {
+          "type" => Array(String),
+          "method" => "unknown_args",
+          "expect_value" => ["3.3"] of String,
+        },
+      ],
+    },
+    {
+      argv:        ["1.1", "2.2", "--array", "array_value", "3.3", "4.4"],
+      expect_help: {{main_help_message}},
+      expect_args: [
+        {
+          "type" => Float32?,
+          "method" => "arg_float_32",
+          "expect_value" => 1.1f32,
+        },
+        {
+          "type" => Float64?,
+          "method" => "arg_float_64",
+          "expect_value" => 2.2f64,
+        },
+        {
+          "type" => Array(String),
+          "method" => "unknown_args",
+          "expect_value" => ["3.3", "4.4"],
+        },
+      ],
+    },
+  ]
+)
+{% end %}
