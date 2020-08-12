d38ffc649037a33354a6706fda039992c899368f
at-grandpa <yoshiyuki.tsuchida.k@gmail.com>
2020-08-12 09:05:04 +0900

    fix

--- spec_completion/bash/bash_spec.cr
+++ spec_completion/bash/bash_spec.cr
@@ -14,15 +14,15 @@ describe "bash completion." do
     {
       input:    "bash_sample -",
       expected: <<-EXPECTED
-      --help        --opt-main-b  -a
-      --opt-main-a  --version     -v
+      --help        --opt-main-b  -a            
+      --opt-main-a  --version     -v            
 
       EXPECTED
     },
     {
       input:    "bash_sample --",
       expected: <<-EXPECTED
-      --help        --opt-main-a  --opt-main-b  --version
+      --help        --opt-main-a  --opt-main-b  --version     
 
       EXPECTED
     },
@@ -36,34 +36,34 @@ describe "bash completion." do
     {
       input:    "bash_sample sub1 ",
       expected: <<-EXPECTED
-      --help            -a                sub1_sub2
-      --opt-sub1-a      -h                sub1_sub2_alias1
-      --opt-sub1-b      sub1_sub1         sub1_sub2_alias2
+      --help            -a                sub1_sub2         
+      --opt-sub1-a      -h                sub1_sub2_alias1  
+      --opt-sub1-b      sub1_sub1         sub1_sub2_alias2  
 
       EXPECTED
     },
     {
       input:    "bash_sample sub1_alias2 ",
       expected: <<-EXPECTED
-      --help            -a                sub1_sub2
-      --opt-sub1-a      -h                sub1_sub2_alias1
-      --opt-sub1-b      sub1_sub1         sub1_sub2_alias2
+      --help            -a                sub1_sub2         
+      --opt-sub1-a      -h                sub1_sub2_alias1  
+      --opt-sub1-b      sub1_sub1         sub1_sub2_alias2  
 
       EXPECTED
     },
     {
       input:    "bash_sample sub1 sub1_sub2 ",
       expected: <<-EXPECTED
-      --help             --opt-sub1-sub2-b  sub1_sub2_sub1
-      --opt-sub1-sub2-a  -a                 sub1_sub2_sub2
+      --help             --opt-sub1-sub2-b  sub1_sub2_sub1     
+      --opt-sub1-sub2-a  -a                 sub1_sub2_sub2     
 
       EXPECTED
     },
     {
       input:    "bash_sample sub1 sub1_sub2_alias2 ",
       expected: <<-EXPECTED
-      --help             --opt-sub1-sub2-b  sub1_sub2_sub1
-      --opt-sub1-sub2-a  -a                 sub1_sub2_sub2
+      --help             --opt-sub1-sub2-b  sub1_sub2_sub1     
+      --opt-sub1-sub2-a  -a                 sub1_sub2_sub2     
 
       EXPECTED
     },
@@ -71,7 +71,7 @@ describe "bash completion." do
       input:    "bash_sample sub1 sub1_sub2_alias2 sub1_sub2_sub2 -",
       expected: <<-EXPECTED
       --help                  --opt-sub1-sub2-sub2-b  -p
-      --opt-sub1-sub2-sub2-a  -a
+      --opt-sub1-sub2-sub2-a  -a                      
 
       EXPECTED
     },
@@ -85,7 +85,6 @@ describe "bash completion." do
   ].each do |spec_case|
     it "program_name: bash_sample, input: #{spec_case[:input]}" do
       command = "docker run --rm -v #{__DIR__}/../..:/tmp -w /tmp crystallang/crystal:latest /bin/bash -c \"make build -C spec_completion/bash/ FILE_NAME=bash_sample PROGRAM_NAME=bash_sample INPUT='#{spec_case[:input]}'\""
-      puts command
       `#{command}`
       File.read("#{__DIR__}/output").should eq spec_case[:expected]
     end
@@ -95,24 +94,24 @@ describe "bash completion." do
     {
       input:    "my_cli ",
       expected: <<-EXPECTED
-      --help        --version     sub1          sub2
-      --opt-main-a  -a            sub1_alias1   sub2_alias1
-      --opt-main-b  -v            sub1_alias2   sub2_alias2
+      --help        --version     sub1          sub2          
+      --opt-main-a  -a            sub1_alias1   sub2_alias1   
+      --opt-main-b  -v            sub1_alias2   sub2_alias2   
 
       EXPECTED
     },
     {
       input:    "my_cli -",
       expected: <<-EXPECTED
-      --help        --opt-main-b  -a
-      --opt-main-a  --version     -v
+      --help        --opt-main-b  -a            
+      --opt-main-a  --version     -v            
 
       EXPECTED
     },
     {
       input:    "my_cli --",
       expected: <<-EXPECTED
-      --help        --opt-main-a  --opt-main-b  --version
+      --help        --opt-main-a  --opt-main-b  --version     
 
       EXPECTED
     },
@@ -126,34 +125,34 @@ describe "bash completion." do
     {
       input:    "my_cli sub1 ",
       expected: <<-EXPECTED
-      --help            -a                sub1_sub2
-      --opt-sub1-a      -h                sub1_sub2_alias1
-      --opt-sub1-b      sub1_sub1         sub1_sub2_alias2
+      --help            -a                sub1_sub2         
+      --opt-sub1-a      -h                sub1_sub2_alias1  
+      --opt-sub1-b      sub1_sub1         sub1_sub2_alias2  
 
       EXPECTED
     },
     {
       input:    "my_cli sub1_alias2 ",
       expected: <<-EXPECTED
-      --help            -a                sub1_sub2
-      --opt-sub1-a      -h                sub1_sub2_alias1
-      --opt-sub1-b      sub1_sub1         sub1_sub2_alias2
+      --help            -a                sub1_sub2         
+      --opt-sub1-a      -h                sub1_sub2_alias1  
+      --opt-sub1-b      sub1_sub1         sub1_sub2_alias2  
 
       EXPECTED
     },
     {
       input:    "my_cli sub1 sub1_sub2 ",
       expected: <<-EXPECTED
-      --help             --opt-sub1-sub2-b  sub1_sub2_sub1
-      --opt-sub1-sub2-a  -a                 sub1_sub2_sub2
+      --help             --opt-sub1-sub2-b  sub1_sub2_sub1     
+      --opt-sub1-sub2-a  -a                 sub1_sub2_sub2     
 
       EXPECTED
     },
     {
       input:    "my_cli sub1 sub1_sub2_alias2 ",
       expected: <<-EXPECTED
-      --help             --opt-sub1-sub2-b  sub1_sub2_sub1
-      --opt-sub1-sub2-a  -a                 sub1_sub2_sub2
+      --help             --opt-sub1-sub2-b  sub1_sub2_sub1     
+      --opt-sub1-sub2-a  -a                 sub1_sub2_sub2     
 
       EXPECTED
     },
@@ -161,7 +160,7 @@ describe "bash completion." do
       input:    "my_cli sub1 sub1_sub2_alias2 sub1_sub2_sub2 -",
       expected: <<-EXPECTED
       --help                  --opt-sub1-sub2-sub2-b  -p
-      --opt-sub1-sub2-sub2-a  -a
+      --opt-sub1-sub2-sub2-a  -a                      
 
       EXPECTED
     },
@@ -174,7 +173,8 @@ describe "bash completion." do
     },
   ].each do |spec_case|
     it "program_name: my_cli, input: #{spec_case[:input]}" do
-      `docker run --rm -v $(PWD):/tmp -w /tmp crystallang/crystal:latest /bin/bash -c "make build -C spec_completion/bash/ FILE_NAME=bash_sample PROGRAM_NAME=my_cli INPUT='#{spec_case[:input]}'"`
+      command = "docker run --rm -v #{__DIR__}/../..:/tmp -w /tmp crystallang/crystal:latest /bin/bash -c \"make build -C spec_completion/bash/ FILE_NAME=bash_sample PROGRAM_NAME=my_cli INPUT='#{spec_case[:input]}'\""
+      `#{command}`
       File.read("#{__DIR__}/output").should eq spec_case[:expected]
     end
   end
