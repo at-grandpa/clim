9eaa632b17055ef71ba449946ce39bdc1c7d0246
at-grandpa <yoshiyuki.tsuchida.k@gmail.com>
2021-04-09 02:16:30 +0900

    fix required.cr

--- spec/clim/stdout_spec/files/required.cr
+++ spec/clim/stdout_spec/files/required.cr
@@ -4,6 +4,8 @@ module Hello
   class Cli < Clim
     main do
       usage "hello <name>"
+      help short: "-h"
+      version "Version 0.1.0", short: "-v"
       option "--prefix <text>", type: String, desc: "Prefix.", required: true
       argument "arg1", type: String, desc: "argument1", required: true
       run do |opts, args|
